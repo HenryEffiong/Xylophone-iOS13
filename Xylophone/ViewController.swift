08cfce2d77ccc58fdabf8de40a654bcf2c7a08d2
Henry Hogan <40265229+HenryEffiong@users.noreply.github.com>
2024-11-29 10:33:16 +0100

    added sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(note: (sender.titleLabel?.text)!)
+    }
     
+    
+    func playSound(note: String) {
+        guard let path = Bundle.main.path(forResource: note, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
